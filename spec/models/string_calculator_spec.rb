a1c499253ef376ebab38997354670c604177e1cc
Abhishek Mishra <abhishek.mishra.codes@gmail.com>
2025-05-26 11:15:09 +0530

    Step 3: added spec to test single number input

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -3,10 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe StringCalculator do
-  context '#add' do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
+    end
+
+    it 'returns the number for a single number input' do
+      expect(calculator.add('2')).to eq(2)
     end
   end
 end
+
+# temp comment - 1 out of 2 failure
+# Failure/Error: expect(calculator.add('2')).to eq(2)
+#        expected: 2
+#             got: 0
